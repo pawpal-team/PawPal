e7af79af697adf7ac985ca52077535315bb94fef
周譽凌 <zhou21935@gmail>
2026-06-07 18:39:10 +0800

    feat: 新增 Footer 手機版排版

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,12 +1,15 @@
 <template>
-  <footer>
-    <div>
-      <div>
-        <img src="../assets/images/PawPal_logo_p.PNG" />
-        <p>為毛孩家庭打造的即時照護平台,24 小時陪伴每一個緊急時刻。</p>
+  <footer class="flex flex-col items-center bg-[#E9EEFD]">
+    <div class="h-[210px] flex flex-col items-center">
+      <div class="pt-8 flex flex-col">
+        <img
+          class="w-[140px] h-[80px] object-contain"
+          src="../../assets/images/PawPal_logo_p.PNG"
+        />
+        <p class="hidden">為毛孩家庭打造的即時照護平台,24 小時陪伴每一個緊急時刻。</p>
       </div>
 
-      <div>
+      <div class="hidden">
         <p>醫療專區</p>
         <ul>
           <li>搜尋醫療院所</li>
@@ -15,7 +18,7 @@
         </ul>
       </div>
 
-      <div>
+      <div class="hidden">
         <p>寵物知識+</p>
         <ul>
           <li>經驗分享</li>
@@ -24,18 +27,18 @@
         </ul>
       </div>
 
-      <div>
-        <p>聯絡我們</p>
-        <div>
-          <img src="../assets/icons/facebook-icon.svg" />
-          <img src="../assets/icons/instagram-icon.svg" />
-          <img src="../assets/icons/email-icon.svg" />
+      <div class="flex flex-col items-center mb-6">
+        <p class="mb-4 mt-3">聯絡我們</p>
+        <div class="flex gap-2">
+          <img class="w-[30px] h-[30px]" src="../../assets/icons/facebook-icon.svg" />
+          <img class="w-[35px] h-[35px]" src="../../assets/icons/instagram-icon.svg" />
+          <img class="w-[35px] h-[35px]" src="../../assets/icons/email-icon.svg" />
         </div>
-        <p>hi@pawpal.tw</p>
+        <p class="hidden">hi@pawpal.tw</p>
       </div>
     </div>
 
-    <div>
+    <div class="h-[50px] w-full flex items-center justify-center">
       <p>© 2026 PawPal.All rights reserved.</p>
     </div>
   </footer>
