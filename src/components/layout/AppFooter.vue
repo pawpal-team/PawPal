59c16892c9787d2b39c4d59263ad833b7bc9b387
周譽凌 <zhou21935@gmail>
2026-06-07 18:14:54 +0800

    feat: 新增 AppFooter 元件基本結構、LOGO 圖片及 SVG icon

---
+++ src/components/layout/AppFooter.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer>
+    <div>
+      <div>
+        <img src="../assets/images/PawPal_logo_p.PNG" />
+        <p>為毛孩家庭打造的即時照護平台,24 小時陪伴每一個緊急時刻。</p>
+      </div>
+
+      <div>
+        <p>醫療專區</p>
+        <ul>
+          <li>搜尋醫療院所</li>
+          <li>AI 即時診斷</li>
+          <li>緊急處置教學</li>
+        </ul>
+      </div>
+
+      <div>
+        <p>寵物知識+</p>
+        <ul>
+          <li>經驗分享</li>
+          <li>衛教文章</li>
+          <li>小知識測驗</li>
+        </ul>
+      </div>
+
+      <div>
+        <p>聯絡我們</p>
+        <div>
+          <img src="../assets/icons/facebook-icon.svg" />
+          <img src="../assets/icons/instagram-icon.svg" />
+          <img src="../assets/icons/email-icon.svg" />
+        </div>
+        <p>hi@pawpal.tw</p>
+      </div>
+    </div>
+
+    <div>
+      <p>© 2026 PawPal.All rights reserved.</p>
+    </div>
+  </footer>
+</template>
