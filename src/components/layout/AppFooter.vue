2e6f198ba8dcdf768d945583ab8d729660bddf3d
周譽凌 <zhou21935@gmail>
2026-06-07 20:01:35 +0800

    feat: 新增 Footer 平板及電腦版 RWD 排版

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,16 +1,18 @@
 <template>
-  <footer class="flex flex-col items-center bg-[#E9EEFD]">
-    <div class="h-[210px] flex flex-col items-center">
-      <div class="pt-8 flex flex-col">
+  <footer class="flex flex-col items-center">
+    <div
+      class="flex flex-col items-center h-[210px] md:flex-row md:items-stretch md:justify-center md:w-full md:max-w-[1400px] md:h-[200px] md:mx-auto md:px-8 md:py-8 md:gap-20"
+    >
+      <div class="flex flex-col pt-8 md:items-start md:pt-0 md:w-[350px]">
         <img
-          class="w-[140px] h-[80px] object-contain"
+          class="w-[140px] h-[80px] object-contain md:w-[180px]"
           src="../../assets/images/PawPal_logo_p.PNG"
         />
-        <p class="hidden">為毛孩家庭打造的即時照護平台,24 小時陪伴每一個緊急時刻。</p>
+        <p class="hidden md:block">為毛孩家庭打造的即時照護平台,24 小時陪伴每一個緊急時刻。</p>
       </div>
 
-      <div class="hidden">
-        <p>醫療專區</p>
+      <div class="hidden md:flex md:flex-col md:items-start md:w-[350px]">
+        <p class="mt-3 mb-4">醫療專區</p>
         <ul>
           <li>搜尋醫療院所</li>
           <li>AI 即時診斷</li>
@@ -18,8 +20,8 @@
         </ul>
       </div>
 
-      <div class="hidden">
-        <p>寵物知識+</p>
+      <div class="hidden md:flex md:flex-col md:items-start md:w-[350px]">
+        <p class="mt-3 mb-4">寵物知識+</p>
         <ul>
           <li>經驗分享</li>
           <li>衛教文章</li>
@@ -27,18 +29,18 @@
         </ul>
       </div>
 
-      <div class="flex flex-col items-center mb-6">
-        <p class="mb-4 mt-3">聯絡我們</p>
+      <div class="flex flex-col items-center mb-6 md:items-start md:w-[350px]">
+        <p class="mt-3 mb-4">聯絡我們</p>
         <div class="flex gap-2">
           <img class="w-[30px] h-[30px]" src="../../assets/icons/facebook-icon.svg" />
           <img class="w-[35px] h-[35px]" src="../../assets/icons/instagram-icon.svg" />
           <img class="w-[35px] h-[35px]" src="../../assets/icons/email-icon.svg" />
         </div>
-        <p class="hidden">hi@pawpal.tw</p>
+        <p class="hidden md:block">hi@pawpal.tw</p>
       </div>
     </div>
 
-    <div class="h-[50px] w-full flex items-center justify-center">
+    <div class="flex items-center justify-center w-full h-[50px]">
       <p>© 2026 PawPal.All rights reserved.</p>
     </div>
   </footer>
