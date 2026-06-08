2267d99034b185cb2d4071aa44631d6b93842203
Wen <zwhsin17@gmail.com>
2026-06-09 01:09:16 +0800

    feat: 完成首頁響應式切版

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import HomeView from './views/HomeView.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
+  <!-- <h1>You did it!</h1> -->
+  <HomeView />
 </template>
 
 <style scoped></style>
