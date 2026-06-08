88fe4385e6550f23a33018b217c42ea156018a0c
lianjie <lianjie0819@gmail.com>
2026-06-09 00:31:17 +0800

    refactor: 將內嵌 SVG 改為外部檔案引入

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,7 +4,7 @@
     <div class="md:hidden">
       <div class="mx-auto flex h-[55px] px-4 items-center justify-between">
         <a href="#" class="items-center">
-          <img src="@/assets/icons/PawPal-logo_p.webp" alt="logo" class="h-10 w-auto" />
+          <img src="@/assets/images/PawPal-logo_p.webp" alt="logo" class="h-10 w-auto" />
         </a>
 
         <button type="button" class="flex h-10 w-10 items-center justify-center">
@@ -16,7 +16,7 @@
     <!-- 平板版&電腦版 -->
     <div class="mx-auto hidden h-17 max-w-5xl items-center px-4 md:flex md:justify-between">
       <a href="#" class="flex items-center">
-        <img src="@/assets/icons/PawPal-logo_p.webp" alt="logo" class="h-17 w-auto" />
+        <img src="@/assets/images/PawPal-logo_p.webp" alt="logo" class="h-17 w-auto" />
       </a>
 
       <div class="flex items-center gap-5 lg:contents">
@@ -29,20 +29,11 @@
               class="group flex cursor-pointer items-center gap-1 transition group-hover:text-[#FFA002]"
             >
               <span>{{ group.label }}</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="size-4 text-[#717182] transition group-hover:text-[#FFA002]"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
-                />
-              </svg>
+              <img
+                src="@/assets/icons/angle-arrow.svg"
+                alt="angle-arrow"
+                class="size-4 arrow-icon transition duration-150"
+              />
             </button>
 
             <div
@@ -67,43 +58,18 @@
             href="#"
             class="flex items-center justify-center rounded-full bg-[#FFA002] px-4 py-2 text-white transition hover:bg-[#e58f04]"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="size-5"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-              />
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
-              />
-            </svg>
+            <img src="@/assets/icons/location.svg" class="size-5" />
             搜尋附近醫院
           </a>
           <a
             href="#"
             class="flex items-center justify-center px-4 py-2 text-[#717182] transition hover:text-[#606060]"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              class="size-6"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-                clip-rule="evenodd"
-              />
-            </svg>
+            <img
+              src="@/assets/icons/member.svg"
+              class="member-icon size-6 transition duration-150"
+              alt="member"
+            />
           </a>
         </div>
       </div>
@@ -133,3 +99,23 @@ const navGroups = [
   },
 ]
 </script>
+
+<style>
+.arrow-icon {
+  filter: invert(46%) sepia(8%) saturate(567%) hue-rotate(202deg) brightness(92%) contrast(88%);
+}
+
+.group:hover .arrow-icon {
+  filter: invert(63%) sepia(95%) saturate(700%) hue-rotate(1deg) brightness(103%) contrast(101%);
+}
+
+.member-icon {
+  filter: brightness(0) invert(47%) sepia(8%) saturate(546%) hue-rotate(202deg) brightness(91%)
+    contrast(87%);
+}
+
+.member-icon:hover {
+  filter: brightness(0) invert(38%) sepia(0%) saturate(1%) hue-rotate(198deg) brightness(94%)
+    contrast(92%);
+}
+</style>
