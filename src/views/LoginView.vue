2dfdad271eca4b2531bb14e593a05b08f3f248ad
Kim <coraluu02@gmail.com>
2026-06-09 23:37:17 +0800

    feat: 新增 LoginView/LoginForm，搬移 AppHeader

---
+++ src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Header from '@/components/layout/AppHeader.vue'
+import Footer from '@/components/layout/AppFooter.vue'
+import LoginForm from '@/modules/auth/LoginForm.vue'
+</script>
+
+<template>
+  <div class="flex min-h-screen flex-col">
+    <Header />
+    <LoginForm class="flex-1" />
+    <Footer />
+  </div>
+</template>
