3d805ba9c0164b53800e9a9595641aa7c43b5600
lianjie <lianjie0819@gmail.com>
2026-06-14 21:37:52 +0800

    feat: 建立 新增病歷紀錄 button

---
+++ src/components/medical/AddMedicalButton.vue
@@ -0,0 +1,22 @@
+<script setup>
+const emit = defineEmits(['click'])
+
+const handleClick = () => {
+  emit('click')
+}
+</script>
+
+<template>
+  <button
+    type="button"
+    class="flex cursor-pointer items-center gap-0.5 md:gap-1 rounded-full bg-[#3dba6e] px-2 py-1 md:px-2.5 md:py-1.5 lg:px-3 lg:py-2 text-xs md:text-sm lg:text-base font-medium text-white transition hover:bg-[#34a660] active:bg-[#2d9455]"
+    @click="handleClick"
+  >
+    <img
+      class="w-2 h-2 md:w-3 md:h-3 lg:w-3.5 lg:h-3.5"
+      src="@/assets/icons/plus_w.svg"
+      alt="addRecord"
+    />
+    新增紀錄
+  </button>
+</template>
