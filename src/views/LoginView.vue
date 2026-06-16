b437fa4fde52540e262ec03f89a824d4b267d915
Kim <coraluu02@gmail.com>
2026-06-16 21:11:52 +0800

    feat: 新增登入表單，emit 帳號密碼至父元件

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,23 @@
 <script setup>
+import { useRouter } from 'vue-router'
 import Header from '@/components/layout/AppHeader.vue'
 import Footer from '@/components/layout/AppFooter.vue'
 import LoginForm from '@/components/auth/LoginForm.vue'
+import { useAuthStore } from '@/stores/auth.js'
+
+const router = useRouter()
+const authStore = useAuthStore()
+
+function handleLogin(payload) {
+  authStore.login(payload.email, payload.password)
+  router.push('/dashboard')
+}
 </script>
 
 <template>
   <div class="flex min-h-screen flex-col pt-[55px] lg:pt-[68px]">
     <Header />
-    <LoginForm class="flex-1" />
+    <LoginForm class="flex-1" @login="handleLogin" />
     <Footer />
   </div>
 </template>
