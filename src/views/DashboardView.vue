971289b8bd363e7f28708b0c29ef5752ebcbcd7a
C-1-HAO <yam60105@gmail.com>
2026-06-17 14:46:44 +0800

    feat: 支援 Header 依登入狀態切換 Sidebar

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useRouter } from 'vue-router'
-import DashboardSidebar from '@/components/layout/DashboardSidebar.vue'
+import AppHeader from '@/components/layout/AppHeader.vue'
 import { useAuthStore } from '@/stores/auth.js'
 
 const router = useRouter()
@@ -13,12 +13,15 @@ function handleLogout() {
 </script>
 
 <template>
-  <div>Dashboard（施工中）</div>
-  <button
-    type="button"
-    class="rounded-full bg-brand-orange px-5 py-2 text-white"
-    @click="handleLogout"
-  >
-    登出
-  </button>
+  <AppHeader variant="member" />
+  <main class="min-h-screen pt-[55px] lg:pl-52 lg:pt-[68px]">
+    <div>Dashboard（施工中）</div>
+    <button
+      type="button"
+      class="rounded-full bg-brand-orange px-5 py-2 text-white"
+      @click="handleLogout"
+    >
+      登出
+    </button>
+  </main>
 </template>
