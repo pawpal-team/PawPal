dc7b9fea37430fa4ea89badb27d325b299b434fb
Kim <coraluu02@gmail.com>
2026-06-19 00:23:04 +0800

    feat: 建立註冊路由

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import authRoutes from './routes/auth.routes.js'
 
 dotenv.config()
 
@@ -14,6 +15,7 @@ app.use(
   }),
 )
 app.use(express.json())
+app.use('/api/auth', authRoutes)
 
 app.get('/', (req, res) => {
   res.json({
