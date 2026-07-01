a907718f945a64d2a3d257b63b0154791681e491
周譽凌 <zhou21935@gmail>
2026-07-01 23:22:45 +0800

    refactor: 統一 main.js 引入路徑為 @

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
-import './assets/main.css'
+import App from '@/App.vue'
+import router from '@/router'
+import '@/assets/main.css'
 
 import SetupCalendar from 'v-calendar'
 import 'v-calendar/style.css'
